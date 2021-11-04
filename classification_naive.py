321d05c1dc55392c3864e475896d6fa4cb70aeeb
peterkimathi <44608801+peterkimathi@users.noreply.github.com>
2021-11-04 12:10:41 +0200

    Update classification_naive.py

--- classification_naive.py
+++ classification_naive.py
@@ -2,7 +2,6 @@ import pandas as pd
 from Bio import SeqIO
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB,MultinomialNB, BernoulliNB
-from sklearn.model_selection import train_test_split
 
 gnb = GaussianNB()
 multinb=MultinomialNB()
